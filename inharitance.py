d8ea8ee854d2341f21ff6c6a442315f76fcc5519
Lahiru167 <lahirushehan@2002gmail.com>
2025-12-02 23:33:22 +0530

    Refactor fruit classes and implement pricing functionality

--- inharitance.py
+++ inharitance.py
@@ -1,12 +1,43 @@
-class parent:
-    def fun1(self):
-        print("Hello")
+class friut:
+    numberOfitems = None
+    Unit_price = None
+    def set_Value(self,x,y):
+        self.numberOfitems=x
+        self.Unit_price=y
 
-class child(parent):
-    def fun2(self):
-        print("welcome")
-    def fun1(self):
-        print("Hi")
+class apple(friut):
+    
+    def price(self):
+        print("Total price of apple:",self.numberOfitems*self.Unit_price)
+class Orange(friut):
+    
+    def price(self):
+        print("Total price of Orange:",self.numberOfitems*self.Unit_price)
 
-obj = child()
-obj.fun1()
+class grapes(friut):
+    
+    def price(self):
+        print("Total price of grapes:",self.numberOfitems*self.Unit_price)
+        
+
+obj1=apple()
+obj2=Orange()
+obj3=grapes()
+
+numberOfitems = int(input("Enter number of items of apple: "))
+Unit_price = int(input("Enter unit price of apple: "))
+obj1.set_Value(numberOfitems,Unit_price)
+
+numberOfitems = int(input("Enter number of items of Orange: "))
+Unit_price = int(input("Enter unit price of Orange: "))
+obj2.set_Value(numberOfitems,Unit_price)
+
+numberOfitems = int(input("Enter number of items of grapes: "))
+Unit_price = int(input("Enter unit price of grapes: "))
+obj3.set_Value(numberOfitems,Unit_price)
+
+
+
+obj1.price()
+obj2.price()
+obj3.price()
